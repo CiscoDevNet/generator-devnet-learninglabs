3603fa24b95ea9e40ccf7384ae49ac40e26ed14a
Ashley Roach <aroach@gmail.com>
2016-05-13 18:27:55 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# generator-devnet-learninglabs [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> Generator for DevNet Learning Labs
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-devnet-learninglabs using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-devnet-learninglabs
+```
+
+Then generate your new project:
+
+```bash
+yo devnet-learninglabs
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+MIT © [Ashley Roach](https://developer.cisco.com)
+
+
+[npm-image]: https://badge.fury.io/js/generator-devnet-learninglabs.svg
+[npm-url]: https://npmjs.org/package/generator-devnet-learninglabs
+[travis-image]: https://travis-ci.org/CiscoDevNet/generator-devnet-learninglabs.svg?branch=master
+[travis-url]: https://travis-ci.org/CiscoDevNet/generator-devnet-learninglabs
+[daviddm-image]: https://david-dm.org/CiscoDevNet/generator-devnet-learninglabs.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/CiscoDevNet/generator-devnet-learninglabs
---
+++ generators/app/index.js
@@ -0,0 +1,78 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.Base.extend({
+  prompting: function () {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the ' + chalk.red('DevNet Learning Labs') + ' generator!'
+    ));
+
+    var prompts = [
+      {
+        type: 'input',
+        name: 'labId',
+        message: 'Pick a lab ID (this must not contain spaces, but hyphens are OK)'
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title for your lab?'
+      },
+      {
+        type: 'input',
+        name: 'slug',
+        message: 'What is the description for your lab?'
+      },
+      {
+        type: 'input',
+        name: 'tags',
+        message: 'What some tags for your lab?'
+      },
+      {
+        type: 'input',
+        name: 'length',
+        message: 'How long will your learning lab be (in minutes)?',
+        default: '30'
+      },
+      {
+        type: 'input',
+        name: 'author_name',
+        message: 'What is your name?'
+      },
+      {
+        type: 'input',
+        name: 'author_email',
+        message: 'What is your email address?'
+      }
+    ];
+
+    return this.prompt(prompts).then(function (props) {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    }.bind(this));
+  },
+
+  writing: function () {
+    var context = {
+      title: this.props.title,
+      labId: this.props.labId,
+      slug: this.props.slug,
+      tags: this.props.tags,
+      author_name: this.props.author_name,
+      author_email: this.props.author_email,
+      length: this.props.length
+    };
+    this.copy('sample-lab/byod.html', this.props.labId + '/byod.html', context);
+    this.template('sample-lab/1.md', this.props.labId + '/1.md', context);
+    this.template('sample-lab/2.md', this.props.labId + '/2.md', context);
+    this.template('sample-lab/sample-lab.json', this.props.labId + '/' + this.props.labId + '.json', context);
+  },
+  
+  end: function () {
+    this.log('You now have a template Cisco DevNet Learning Lab.  If you add pages, you\'ll have to manually add them to the JSON file.');
+  }
+
+});
